8a21bc79015fa86114a55158b1f879789d07fff6
Andrés Geovanny Angulo Botina <andrescabj981@gmail.com>
2021-10-10 14:29:12 -0500

    Updates

---
+++ precache-manifest.c2566b430c4a5aa5be75bf3acec4f1d2.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d5cb89a69dc0ab2144eb1060b8e374c",
+    "url": "/mi-liga/index.html"
+  },
+  {
+    "revision": "4ae48836180533150980",
+    "url": "/mi-liga/static/css/main.b26ed110.chunk.css"
+  },
+  {
+    "revision": "a6ec1f5f1bcdcfcbb6b9",
+    "url": "/mi-liga/static/js/2.333c2eed.chunk.js"
+  },
+  {
+    "revision": "d140213c99598621a8b4e82fad7572a4",
+    "url": "/mi-liga/static/js/2.333c2eed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ae48836180533150980",
+    "url": "/mi-liga/static/js/main.edc11955.chunk.js"
+  },
+  {
+    "revision": "8cc68e1af4b899a5681d",
+    "url": "/mi-liga/static/js/runtime-main.9cf45413.js"
+  },
+  {
+    "revision": "6b577104bb5726919dab8cff25851555",
+    "url": "/mi-liga/static/media/ernesto.6b577104.png"
+  },
+  {
+    "revision": "ec070ff55ca2286ff533cfd7cb1689ae",
+    "url": "/mi-liga/static/media/felinos.ec070ff5.png"
+  },
+  {
+    "revision": "4fe9d27494f3f26700bf6fd576f24721",
+    "url": "/mi-liga/static/media/hugo.4fe9d274.png"
+  },
+  {
+    "revision": "b4a99d0159a6ec68cd91a4465369a905",
+    "url": "/mi-liga/static/media/jose.b4a99d01.png"
+  },
+  {
+    "revision": "cc9e20dc36b0cf4c5bfda95a0a8493db",
+    "url": "/mi-liga/static/media/juan.cc9e20dc.png"
+  },
+  {
+    "revision": "760178851d8c4d3ff93da020fc619656",
+    "url": "/mi-liga/static/media/luis.76017885.png"
+  },
+  {
+    "revision": "567afa70e3eb93737f13a9c0f3f232cd",
+    "url": "/mi-liga/static/media/manuel.567afa70.png"
+  },
+  {
+    "revision": "db573ac5894b52557a5e624b1f8d66bf",
+    "url": "/mi-liga/static/media/mi-liga-2.db573ac5.png"
+  },
+  {
+    "revision": "939507d89b1546ccbb15563b6d66cbdb",
+    "url": "/mi-liga/static/media/mi-liga.939507d8.png"
+  },
+  {
+    "revision": "392f84eb322df4c270e94b351110f16b",
+    "url": "/mi-liga/static/media/paco.392f84eb.png"
+  },
+  {
+    "revision": "e7b4175e4db9c3db98f5ae92e19bf5c8",
+    "url": "/mi-liga/static/media/piratas.e7b4175e.png"
+  },
+  {
+    "revision": "eb85c8dc5ab5026ac7119e828f12a4de",
+    "url": "/mi-liga/static/media/sergio.eb85c8dc.png"
+  },
+  {
+    "revision": "9752a81329c2504d71ced85663f5e70e",
+    "url": "/mi-liga/static/media/tiburones.9752a813.png"
+  }
+]);
